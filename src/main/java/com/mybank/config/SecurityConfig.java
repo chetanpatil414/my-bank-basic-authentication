552e0ecdd7b187faf9e51548ccf1a2f74a6f4654
chetanpatil414 <chetanpatil414@gmail.com>
2024-09-27 22:58:02 +0530

    new api created for custom authentication.

--- src/main/java/com/mybank/config/SecurityConfig.java
+++ src/main/java/com/mybank/config/SecurityConfig.java
@@ -2,27 +2,21 @@ package com.mybank.config;
 
 import com.mybank.exceptionhandling.CustomBasicAuthenticationEntryPoint;
 import com.mybank.filter.CsrfCookieFilter;
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
-import org.springframework.security.authentication.password.CompromisedPasswordChecker;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.HttpStatusEntryPoint;
-import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
 
 import java.util.Collections;
 
@@ -35,28 +29,25 @@ public class SecurityConfig {
         requestHandle.setCsrfRequestAttributeName("_csrf");
         httpSecurity.securityContext(contextConfig-> contextConfig.requireExplicitSave(false))
                 .sessionManagement(sessionConfig->sessionConfig.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
-                .cors(corsCustomizer->corsCustomizer.configurationSource(new CorsConfigurationSource() {
-            @Override
-            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
-                CorsConfiguration corsConfiguration = new CorsConfiguration();
-                corsConfiguration.setAllowedOrigins(Collections.singletonList("http://localhost:3000/"));
-                corsConfiguration.setAllowedMethods(Collections.singletonList("*"));
-                corsConfiguration.setAllowCredentials(true);
-                corsConfiguration.setAllowedHeaders(Collections.singletonList("*"));
-                corsConfiguration.setMaxAge(3600l);
+                .cors(corsCustomizer->corsCustomizer.configurationSource(request -> {
+                    CorsConfiguration corsConfiguration = new CorsConfiguration();
+                    corsConfiguration.setAllowedOrigins(Collections.singletonList("http://localhost:3000/"));
+                    corsConfiguration.setAllowedMethods(Collections.singletonList("*"));
+                    corsConfiguration.setAllowCredentials(true);
+                    corsConfiguration.setAllowedHeaders(Collections.singletonList("*"));
+                    corsConfiguration.setMaxAge(3600L);
 
-                return corsConfiguration;
-            }
-        })).csrf(csrf->csrf.csrfTokenRequestHandler(requestHandle)
-                .ignoringRequestMatchers("/", "/user")
+                    return corsConfiguration;
+                })).csrf(csrf->csrf.csrfTokenRequestHandler(requestHandle)
+                .ignoringRequestMatchers("/", "/user","/logIn")
                 .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
                 .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class);
 
 
 
-        httpSecurity.authorizeHttpRequests(req->req.requestMatchers("/dashboard","/logIn").authenticated()
-                .requestMatchers("/","/user").permitAll());
-        httpSecurity.formLogin(flc->flc.disable());
+        httpSecurity.authorizeHttpRequests(req->req.requestMatchers("/dashboard").authenticated()
+                .requestMatchers("/","/user","/logIn").permitAll());
+        httpSecurity.formLogin(AbstractHttpConfigurer::disable);
         httpSecurity.httpBasic(hbc->hbc.authenticationEntryPoint(new CustomBasicAuthenticationEntryPoint()));
 
 
--- src/main/java/com/mybank/controller/UserController.java
+++ src/main/java/com/mybank/controller/UserController.java
@@ -1,36 +1,36 @@
 package com.mybank.controller;
 
-import com.mybank.config.MyBankUserDetailService;
 import com.mybank.model.LoginRequestDTO;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class UserController {
-    @Autowired
-    MyBankUserDetailService myBankUserDetailService;
-
-    @Autowired
-    PasswordEncoder passwordEncoder;
-
 
    private final AuthenticationManager authenticationManager;
 
     @PostMapping("/logIn")
     public ResponseEntity logIn(@RequestBody LoginRequestDTO details){
-        UsernamePasswordAuthenticationToken unauthenticated = UsernamePasswordAuthenticationToken.unauthenticated(details.userName(), details.Password());
-        Authentication authenticate = authenticationManager.authenticate(unauthenticated);
-        SecurityContextHolder.getContext().setAuthentication(authenticate);
-        return ResponseEntity.ok("Login Successful");
+        ResponseEntity result;
+        try {
+            UsernamePasswordAuthenticationToken unauthenticated = UsernamePasswordAuthenticationToken.unauthenticated(details.userName(), details.Password());
+            Authentication authenticate = authenticationManager.authenticate(unauthenticated);
+            SecurityContextHolder.getContext().setAuthentication(authenticate);
+            result = ResponseEntity.ok("Login Successful");
+        }catch (AuthenticationException ex){
+            result = ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials");
+        }
+        return result;
     }
 
 }
